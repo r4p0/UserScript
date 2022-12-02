f3e9fa24f92594e785a9aa3c59d48367abb833b7
OXX <jimycc@163.com>
2022-12-02 17:44:05 +0800

    Remover gray filter for all website

---
+++ disable_gray.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         去除灰色
+// @namespace    https://github.com/r4p0/UserScript
+// @version      0.1
+// @description  Remover gray filter for all website
+// @author       r4p0
+// @match        *://***/*
+// @supportURL   https://github.com/r4p0/UserScript
+// @homepageURL  https://github.com/r4p0/UserScript
+// @updateURL    https://r4p0.github.io/UserScript/disable_infinite_debugger.user.js
+// @downloadURL  https://r4p0.github.io/UserScript/disable_infinite_debugger.user.js
+// @run-at       document-end
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    GM_addStyle(`*{filter:unset !important;-webkit-filter:unset !important;}`);
+})();
